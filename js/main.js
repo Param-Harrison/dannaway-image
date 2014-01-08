807847ffbc0eb8665d8cd8a7c7101c8fee80d6c3
Param-Harrison <paramanantham@live.com>
2014-01-08 19:09:52 +0530

    changes animate func

--- js/main.js
+++ js/main.js
@@ -1,46 +1,58 @@
 $(function() {
-	var currentMousePos = { x: -1, y: -1 };
-    $(document).mousemove(function(event) {
-        currentMousePos.x = event.pageX;
-        currentMousePos.y = event.pageY;
-    });
-	var $element = $('.portfolio'),
-		$coder = $('.coder-img'),
-		$color = $('.color-img');
+	animateFace();
+	
+	/* 
+	* Function to animate face
+	*/
+	function animateFace() {
+		var designerImg 	= $('#designer-img');
+		var coderImg 		= $('#coder-img');
+		var designerHover	= $('#designer');		
+		var designerBg		= $('#designer-bg');
+		var coderBg			= $('#coder-bg');
+		var face 			= $('#face');
+		var designerDesc	= $('#designer-desc');
+		var coderDesc		= $('#coder-desc');
+		var section 		= $(document);
+		var duration 		= 500;
 
-	$element.mouseover(function() {
-		var width = $(this).width();
-		var height = $(this).height();
-
-		if(currentMousePos.y <= height) {
-			var diff = Math.abs(currentMousePos.x - width / 2);
-			if(currentMousePos.x <= width / 2) {
-				//Coder
-				var width = $coder.width();
-				var dataWid = $coder.data('width');
-				if(width + dataWid > dataWid * 2) width = dataWid * 2;
-				else width += diff;
-				$coder.animate({ width: width }, 1000);
-				var diffWidth = Math.abs(width - diff);
-				if(diffWidth < 0) diffWidth = 0;
-				$color.animate({ width: diffWidth }, 1000);
-			} 
-			else {
-				//Color
-				var width = $color.width();
-				var dataWid = $color.data('width');
-				if(width + dataWid > dataWid * 2) width = dataWid * 2;
-				else width += diff;
-				$color.animate({ width: width }, 1000);
-				var diffWidth = Math.abs(width - diff);
-				if(diffWidth < 0) diffWidth = 0;
-				$coder.animate({ width: diffWidth }, 1000);
-			}
-		}
-		else{
-			// Normal Condition
-			$coder.animate({ width: $coder.data('width') }, 1000);
-			$color.animate({ width: $color.data('width') }, 1000);
-		}
-	});
+		var mouseX = 0;
+		var relMouseX = 520;
+		var xp = 520;
+		frameRate =  30;
+		timeInterval = Math.round( 1000 / frameRate );		
+		
+		section.mouseenter(function(e) {
+			// Get mouse position
+			section.mousemove(function(e) {
+				// raw mouse position
+				mouseX = e.pageX;
+				// mouse position relative to face div
+				relMouseX = mouseX - face.offset().left;
+			});
+			// Animate the face based on mouse movement
+			loop = setInterval(function(){
+				// zeno's paradox dampens the movement
+				xp += (relMouseX - xp) / 12;
+				designerImg.css({width:420 + (520 - xp) * 0.5, left: 100 + (520 - xp) * 0.1});
+				coderImg.css({width:420 + (xp - 520) * 0.5, right: 100 - (520 - xp) * 0.1});
+				designerBg.css({left: 100 + (520 - xp) * 0.05, opacity: ((1040 - xp)/520)});
+				coderBg.css({right:  100 + (xp - 520) * 0.05, opacity: (xp/520)});
+				designerDesc.css({opacity: ((1040 - xp)/520)});
+				coderDesc.css({opacity: (xp/520)});
+			}, timeInterval);
+		}).mouseleave(function(e) { 
+			// reset the face to initial state
+			clearInterval(loop);
+			xp 			= 520;
+			mouseX 		= 0;
+			relMouseX 	= 520;
+			designerImg.animate({width: 420, left: 100}, duration);
+			coderImg.animate({width: 420, right: 100}, duration);
+			coderDesc.animate({opacity: 1}, duration);
+			designerDesc.animate({opacity: 1}, duration);
+			coderBg.animate({right:100, opacity: 1}, duration);
+			designerBg.animate({left:100, opacity: 1}, duration);
+		}); 
+	}; 
 });
